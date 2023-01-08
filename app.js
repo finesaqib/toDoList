25869370062012a00ded6036327a0c9dcad12d80
Caqib Qureshi <finesaqib@yahoo.com>
2023-01-08 20:19:33 +0530

    Port

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const listSchema = {
   Items: [itemsSchema]
 }
 let port = process.env.PORT;
-if (port === null  || port = "") {
+if (port = null  || port = "") {
   post = 3000
 }
 const Item = mongoose.model( "Item", itemsSchema);
